e014df20726073c07d5ea13e3755cfd13c98d8ad
mbabaliiryn <mbabaliiryn@gmail.com>
2022-02-15 15:48:57 +0300

    add admin and user login

---
+++ save/models.py
@@ -0,0 +1,19 @@
+
+from random import randrange
+
+from django.contrib import admin
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+   
+    pass
+    # add additional fields in here
+    
+
+    def __str__(self):
+        
+        return self.username
+
+
